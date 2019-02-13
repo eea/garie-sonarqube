5cff52925025f50517f271c20d028bcf62f35916
valentinab25 <valentinab25>
2019-02-13 10:32:36 +0200

    Remove token obligativity

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,18 +2,11 @@
 set -e
 
 
-if [ -z "$SONARQUBE_TOKEN" ]; then
-        echo "Please provide the plugin keys in the UPTIME_ROBOT_KEYS variable"
-        exit 1
-fi
-
 
 if [ -z "$SONARQUBE_API_URL" ]; then
         echo "SONARQUBE_API_URL not found"
         exit 1
 fi
-
-
 
 
 if [ -n "$CONFIG" ]; then
